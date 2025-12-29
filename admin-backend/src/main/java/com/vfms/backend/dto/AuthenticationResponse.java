91cefb2a31885f19bff8f4fcecf575a9f046d81b
Kiruthiyan <kiruthiyan7@gmail.com>
2025-12-29 19:14:42 +0530

    Initial commit for admin portal and backend

---
+++ admin-backend/src/main/java/com/vfms/backend/dto/AuthenticationResponse.java
@@ -0,0 +1,16 @@
+package com.vfms.backend.dto;
+
+import com.vfms.backend.entity.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+    private String role;
+}
---
+++ admin-backend/src/test/java/com/vfms/VfmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vfms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VfmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
